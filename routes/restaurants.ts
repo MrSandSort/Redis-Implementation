f7b55ede2d6d6a598b93e5dc81ad816907c7ec1b
DevSandeshPaudel <paudelsandesh12000@gmail.com>
2025-03-10 16:44:18 +0545

    added the redis addition and fetching of data from redis

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,15 +1,42 @@
- import express from "express";
+ import express,{type Request} from "express";
 import { Restaurant, RestaurantSchema } from "../Schemas/restaurants.schema";
 import { validate } from "../middleware/validate";
 import { successResponse } from "../utils/responses";
 import { initializeRedisClient } from "../utils/client";
+import { restaurantById } from "../utils/keys";
+import { nanoid } from "nanoid";
 
  const router= express.Router()
 
- router.post('/', validate(RestaurantSchema), async(req,res)=>{
+ router.post('/', validate(RestaurantSchema), async(req,res, next)=>{
     const data= req.body as Restaurant
-    const client= await initializeRedisClient();
-    successResponse(res, data, "Restaurant Added successfully")
+
+    try {
+      const client= await initializeRedisClient();
+      const id= nanoid();
+      const restaurantKey= restaurantById(id)
+      const hashData= {id, name:data.name, location:data.location}
+      const addResult= await client.hSet(restaurantKey, hashData)
+      successResponse(res, hashData, "Restaurant Added successfully")
+      console.log(hashData)
+    } catch (error) {
+      next(error);
+    }
+  
  })
 
+ router.get('/:restaurantId', async(req:Request<{restaurantId:string}>, res, next)=>{
+   const {restaurantId}= req.params
+
+   try {
+
+      const client= await initializeRedisClient();
+      const restaurantKey= restaurantById(restaurantId);
+      const restaurant= await client.hGetAll(restaurantKey);
+      successResponse(res,restaurant);
+   } catch (error) {
+      next(error)
+   }
+
+ })
  export default router;
