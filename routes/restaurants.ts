f42a31027b32e3a38e90c6884dca30869abce819
DevSandeshPaudel <paudelsandesh12000@gmail.com>
2025-03-10 12:51:30 +0545

    successfully set up the redis

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,5 +1,15 @@
  import express from "express";
+import { Restaurant, RestaurantSchema } from "../Schemas/restaurants.schema";
+import { validate } from "../middleware/validate";
+import { successResponse } from "../utils/responses";
+import { initializeRedisClient } from "../utils/client";
 
  const router= express.Router()
+
+ router.post('/', validate(RestaurantSchema), async(req,res)=>{
+    const data= req.body as Restaurant
+    const client= await initializeRedisClient();
+    successResponse(res, data, "Restaurant Added successfully")
+ })
 
  export default router;
